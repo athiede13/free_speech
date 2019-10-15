6c0906779c3aa53ddcf5d20d1beff5bc10a702cc
Anja Thiede <anja.thiede@helsinki.fi>
2019-10-15 12:27:58 +0300

    update to more automatic

--- stats/mantel/cluster_correction.py
+++ stats/mantel/cluster_correction.py
@@ -13,6 +13,7 @@ from scipy.io import loadmat
 import matplotlib.pyplot as plt
 import mne
 from mne import spatial_src_connectivity
+import os
 
 from permutation_cluster_test_AT import _permutation_cluster_test_AT
 
@@ -45,7 +46,7 @@ np.save(save_dir + 'connectivity', connectivity_sparse)
 #      or by the sum of voxel t-values within the cluster) to build the null distribution.
 
 # define conditions
-mode = 'iq' # 'iq' or 'read' or 'mem' or 'phon'
+modes = {'iq', 'read', 'mem', 'phon'}
 cons = '_1' # '_1' listening to speech
 freqs = {'5.000000e-01-4Hz', '4-8Hz', '8-12Hz', '12-25Hz', '25-45Hz', '55-90Hz'}
 
@@ -56,94 +57,102 @@ elif cons == '_2':
 else:
     print('Check condition!')
 
-for freq in freqs:
-    print(mode + ' ' + cons + ' ' + freq)
-    # read in surrogate and actual mantel test results
-    surrogate_values = loadmat(read_dir + mode + '_surrogate_values_' +
-                               freq + window + cons + '.mat')['surrogate_values']
-    r_mantel = loadmat(read_dir + mode + '_r_mantel_' + freq + window + cons +
-                       '.mat')['r_mantel']
-
-    # get threshold
-    threshold = loadmat(read_dir + mode + '_rthreshold_uncorrected_' + freq +
-                        window + cons + '.mat')['rthreshold_uncorrected']
-    thres_id = threshold.shape[0] - 1 # which frequency band id -1 to get correct index
-
-    # reshape surrogate_values to (n_observations, n_times, n_vertices)
-    surrogate_values = surrogate_values[:, :, np.newaxis]
-    surrogate_values = surrogate_values.reshape((5000, 1, 20484))
-
-    # reshape r_mantel
-    r_mantel = r_mantel[:, :, np.newaxis]
-    r_mantel = r_mantel.reshape((1, 1, 20484))
-
-    # search for clusters
-    max_clu_lens, clusters = _permutation_cluster_test_AT(surrogate_values,
-                                                          threshold=threshold[thres_id][0][0][0],
-                                                          n_permutations=5000,
-                                                          tail=0,
-                                                          connectivity=connectivity_sparse,
-                                                          n_jobs=4, seed=10,
-                                                          max_step=1, t_power=1,
-                                                          out_type='indices',
-                                                          exclude=None,
-                                                          step_down_p=0,
-                                                          check_disjoint=False,
-                                                          buffer_size=1000)
-
-    np.save(results_dir + mode + '_max_clu_lens_' + freq + window + cons, max_clu_lens)
-
-    max_clu_lens = np.load(results_dir + mode + '_max_clu_lens_' + freq + window + cons + '.npy')
-
-    # null distribution
-    plt.figure(0)
-    plt.hist(max_clu_lens)
-    kde = stats.gaussian_kde(max_clu_lens)
-    x = np.linspace(max_clu_lens.min(), max_clu_lens.max(), 100)
-    p = kde(x)
-    # cutoff for a cluster size that is significant
-    plt.figure(1)
-    plt.plot(x, p)
-    plt.hlines(0.05, 0, 14) # visualization of cutoff
-    cutoff = 7 # manually from plot above and kde(x)
-
-    max_clu_lens2, clusters = _permutation_cluster_test_AT(r_mantel,
-                                                           threshold=threshold[thres_id][0][0][0],
-                                                           n_permutations=1,
-                                                           tail=0,
-                                                           connectivity=connectivity_sparse,
-                                                           n_jobs=4, seed=10,
-                                                           max_step=1,
-                                                           t_power=1,
-                                                           out_type='indices',
-                                                           exclude=None,
-                                                           step_down_p=0,
-                                                           check_disjoint=False,
-                                                           buffer_size=1000)
-
-    # length of all initial clusters
-    clu_lens = np.zeros(len(clusters))
-    for j in range(0, len(clusters)):
-        clu_lens[j] = len(clusters[j][0])
-
-    # get p-values for all clusters
-    p = kde(clu_lens)
-
-    # out in format required by MNE cluster function (for visualization)
-    r_mantel_out = r_mantel.reshape(1, 20484)
-    clusters_new = clusters
-    for c, l, i in zip(clusters, clu_lens, range(0, len(clusters))):
-        clusters_new[i] = np.zeros(np.int(l), dtype='int'), c[0]
-
-    clu = r_mantel_out, clusters_new, p
-    np.save(results_dir + mode + '_clu_' + freq + window + cons, clu)
-
-    # see how many clusters exceed the threshold (i.e. survive the correction)
-    ids = np.where(clu_lens > cutoff)[0]
-    clu_sig = clusters[0:len(ids)]
-    for i in range(0, len(ids)):
-        clu_sig[i] = clusters[ids[i]]
-
-    sig_clu_lens = np.zeros(len(clu_sig))
-    for j in range(0, len(clu_sig)):
-        sig_clu_lens[j] = len(clu_sig[j][0])
+for mode in modes:
+    for freq in freqs:
+        if os.path.isfile(read_dir + mode + '_rthreshold_uncorrected_' + freq + window + cons 
+                          + '.mat'):
+            print(mode + ' ' + cons + ' ' + freq)
+            # read in surrogate and actual mantel test results
+            surrogate_values = loadmat(read_dir + mode + '_surrogate_values_' +
+                                       freq + window + cons + '.mat')['surrogate_values']
+            r_mantel = loadmat(read_dir + mode + '_r_mantel_' + freq + window + cons +
+                               '.mat')['r_mantel']
+        
+            # get threshold
+            threshold = loadmat(read_dir + mode + '_rthreshold_uncorrected_' + freq +
+                                window + cons + '.mat')['rthreshold_uncorrected']
+            thres_id = threshold.shape[0] - 1 # which frequency band id -1 to get correct index
+        
+            # reshape surrogate_values to (n_observations, n_times, n_vertices)
+            surrogate_values = surrogate_values[:, :, np.newaxis]
+            surrogate_values = surrogate_values.reshape((5000, 1, 20484))
+        
+            # reshape r_mantel
+            r_mantel = r_mantel[:, :, np.newaxis]
+            r_mantel = r_mantel.reshape((1, 1, 20484))
+        
+            # search for clusters
+            max_clu_lens, clusters = _permutation_cluster_test_AT(surrogate_values,
+                                                                  threshold=threshold[thres_id][0][0][0],
+                                                                  n_permutations=5000,
+                                                                  tail=0,
+                                                                  connectivity=connectivity_sparse,
+                                                                  n_jobs=4, seed=10,
+                                                                  max_step=1, t_power=1,
+                                                                  out_type='indices',
+                                                                  exclude=None,
+                                                                  step_down_p=0,
+                                                                  check_disjoint=False,
+                                                                  buffer_size=1000)
+        
+            np.save(results_dir + mode + '_max_clu_lens_' + freq + window + cons, max_clu_lens)
+        
+            max_clu_lens = np.load(results_dir + mode + '_max_clu_lens_' + freq + window + cons + '.npy')
+        
+            # null distribution
+            plt.figure(0)
+            plt.hist(max_clu_lens)
+            kde = stats.gaussian_kde(max_clu_lens)
+            x = np.linspace(max_clu_lens.min(), max_clu_lens.max(), 100)
+            p = kde(x)
+            # cutoff for a cluster size that is significant
+            plt.figure(1)
+            plt.plot(x, p)
+            plt.hlines(0.095, 0, 14) # visualization of cutoff
+            # take maximum across all freq bands
+            cutoff = np.max(max_clu_lens)
+            print(cutoff)
+            
+            max_clu_lens2, clusters = _permutation_cluster_test_AT(r_mantel,
+                                                                   threshold=threshold[thres_id][0][0][0],
+                                                                   n_permutations=1,
+                                                                   tail=0,
+                                                                   connectivity=connectivity_sparse,
+                                                                   n_jobs=4, seed=10,
+                                                                   max_step=1,
+                                                                   t_power=1,
+                                                                   out_type='indices',
+                                                                   exclude=None,
+                                                                   step_down_p=0,
+                                                                   check_disjoint=False,
+                                                                   buffer_size=1000)
+        
+            # length of all initial clusters
+            clu_lens = np.zeros(len(clusters))
+            for j in range(0, len(clusters)):
+                clu_lens[j] = len(clusters[j][0])
+        
+            # hists
+            plt.figure(1)
+            plt.hist(max_clu_lens)
+            plt.hist(clu_lens)
+        
+            # out in format required by MNE cluster function (for visualization)
+            r_mantel_out = r_mantel.reshape(1, 20484)
+            clusters_new = clusters
+            for c, l, i in zip(clusters, clu_lens, range(0, len(clusters))):
+                clusters_new[i] = np.zeros(np.int(l), dtype='int'), c[0]
+        
+            clu = r_mantel_out, clusters_new
+            np.save(results_dir + mode + '_clu_' + freq + window + cons, clu)
+        
+            # see how many clusters exceed the threshold (i.e. survive the correction)
+            ids = np.where(clu_lens > cutoff)[0]
+            clu_sig = clusters[0:len(ids)]
+            for i in range(0, len(ids)):
+                clu_sig[i] = clusters[ids[i]]
+        
+            sig_clu_lens = np.zeros(len(clu_sig))
+            for j in range(0, len(clu_sig)):
+                sig_clu_lens[j] = len(clu_sig[j][0])
+        else: print('No uncorrected p-vals < 0.05 for ' + mode + freq)
